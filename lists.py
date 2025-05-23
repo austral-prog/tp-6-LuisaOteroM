829f5a9e05439213ee3c5cab88b024a8b019791a
LuisaOteroM <loteromonsegur@mail.austral.edu.ar>
2025-05-23 16:23:50 -0300

    Update lists.py

--- lists.py
+++ lists.py
@@ -1,43 +1,33 @@
-def remove_elements(lista):
-    
-    nueva_lista = lista[:]
-    
-    if len(nueva_lista) > 5:
-        del nueva_lista[5]  
-    if len(nueva_lista) > 4:
-        del nueva_lista[4]  
-    if len(nueva_lista) > 0:
-        del nueva_lista[0]  
+def remove_elements(lista):#copio la lista para no modificar la original
 
-    return nueva_lista
+    if len(copia_lista_uno) > 5: 
+        del copia_lista_uno[5]  
+    if len(copia_lista_uno) > 4: 
+        del copia_lista_uno[4] 
+        del copia_lista_uno[0]  
 
+    return copia_lista_uno
 
 def add_elements(lista):
+    copia_lista_dos = lista[:]  
+    copia_lista_dos.insert(0, 'Pink') 
+    copia_lista_dos.append('Yellow') 
+    
+    return copia_lista_dos 
 
-    nueva_lista = lista[:]  
-    nueva_lista1 = nueva_lista.insert(0, 'Pink')
-    nueva_lista2 = nueva_lista1.append('Yellow')
-
-    return nueva_lista2
 
 def is_empty(lista):
-
-    if len(lista) == 0:
-        return True
-    else:
-        return False
+    return len(lista) == 0 
 
 
 def check_lists(lista1, lista2):
-   
-    if len(lista1) >= 3 and len(lista2) >= 3:
-        return lista1[2] == lista2[2]
-    else:
-        return False
+    if len(lista1) >= 3 and len(lista2) >= 3: 
+        return lista1[2] == lista2[2] 
+    else: return False
 
 
 def list_of_lists(listas):
-    nueva = []
+    nueva = [] 
 
     if len(listas) >= 1:
         sublista1 = listas[0][:2]  
@@ -45,7 +35,6 @@ def list_of_lists(listas):
         sublista1 = []
     nueva.append(sublista1)
 
-  
     if len(listas) >= 2:
         sublista2 = listas[1][1:4]  
     else:
